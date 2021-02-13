fd343ab2fc021bf45cbbfc40a9bf1b6c330fe0c4
noahmah <nkmah@outlook.com>
2021-02-12 20:16:44 -0800

    fix homescreen conflict

--- comps/Pages/HomeScreen/index.js
+++ comps/Pages/HomeScreen/index.js
@@ -68,28 +68,11 @@ const HomeScreen = ({ onSearchPress }) => {
         { x: -0, y: 0 }
     ])
     const [rounded, setRounded] = useState([])
-<<<<<<< HEAD
     const[sec, setSec] = useState(0)
     const [stateminy, setMinY] = useState(1);
     const [statemaxy, setMaxY] = useState(10);
     const [stateminx, setMinX] = useState(1);
     const [statemaxx, setMaxX] = useState(10);
-=======
-    const [sec, setSec] = useState(0)
-    const [miny, setMinY] = useState(null);
-
-
-    // const [chart, setChart] = useState([{ x: -2, y: 15 },
-    // { x: -1, y: 10 },
-    // { x: 0, y: 12 },
-    // { x: 5, y: 8 },
-    // { x: 6, y: 12 },
-    // { x: 7, y: 14 },
-    // { x: 8, y: 12 },
-    // { x: 9, y: 13.5 },
-    // { x: 10, y: 18 }])
-    // const [rounded, setRounded] = useState([]);
->>>>>>> 50c60a219cbc6a48bfb78c684085ac43a309d80c
     const [allStocks, setAllStocks] = useState();
 
 
